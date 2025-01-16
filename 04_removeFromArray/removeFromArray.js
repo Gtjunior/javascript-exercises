87b3acf5b1eea2469d0697eef7699235fbfb9fd1
Gregory <gqt5051@gmail.com>
2025-01-16 10:31:53 -0500

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums, ...numsToRemove) {
+    // 1. loop thru number's to be removed
+    for(let i = 0; i < numsToRemove.length; i++) {
+        // 2. Assign the number that needs to removed a variable
+        let numToRemove = numsToRemove[i];
+
+        // 3. Check if the number to be removed is within nums
+        while(nums.indexOf(numToRemove) !== -1) {
+            // 4. Assign the index of numToRemove to a variable
+            let numIndex = nums.indexOf(numToRemove);
+            // 5. Remove number
+            nums.splice(numIndex, 1);
+        }
+
+    }
+    // 6. Return nums
+    return nums;
 };
 
 // Do not edit below this line
