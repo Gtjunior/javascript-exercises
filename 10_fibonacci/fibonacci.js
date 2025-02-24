372b443bff5f844736eb08b0510d6020df2f80ed
Gregory <gqt5051@gmail.com>
2025-02-24 16:19:49 -0500

    Create fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(max) {
+    let sequence = [1, 1];
+    let index = 0;
+    let num = Number(max);
 
+    if (num < 0) {
+        return "OOPS";
+    } else if (num === 0) {
+        return 0;
+    }
+
+    while (sequence.length < num) {
+        let firstNum = sequence[index];
+        let secondNum = sequence[index + 1];
+        let sum = firstNum + secondNum;
+
+        sequence.push(sum);
+        index++;
+    }
+
+
+    return sequence[num - 1];
 };
 
 // Do not edit below this line
