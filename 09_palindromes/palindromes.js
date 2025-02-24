ffc728cc3c01dc16482cd3b95dfca084a6ec0639
Gregory <gqt5051@gmail.com>
2025-02-24 15:30:44 -0500

    Create Palidome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let alphanumerical = 'abecdefghijeklmnopqrstuvwxyz0123456789';
+
+    let newStr = str.toLowerCase().split('').filter(char => alphanumerical.includes(char)).join('');
+
+    let reverseStr = newStr.split('').reverse('').join('');
+
+    return newStr === reverseStr;
 };
 
 // Do not edit below this line
